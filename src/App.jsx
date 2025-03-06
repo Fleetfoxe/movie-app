e687e208e6aab66dc0deedb882bdba288ebdfbc8
Peter Åkeson <p.akesson@gmail.com>
2025-03-06 09:27:44 +0100

    added LandingPage and moved logic there

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,11 @@
-import { useState, useEffect } from 'react'
-import { SearchBar } from "./components/SearchBar";
-import { fetchMovies } from "./api/movieApi";
-import { Link } from "react-router";
+import React from 'react';
+import LandingPage from './core/components/LandingPage';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  const handleSearch = async (query) => {
-    const results = await fetchMovies(query);
-    setMovies(results);
-    
-    if (results) {
-      setMovies(results);
-      sessionStorage.setItem("movies", JSON.stringify(results));
-    }
-  };
-
-  useEffect(() => {
-    const savedMovies = sessionStorage.getItem("movies");
-    if (savedMovies) {
-      setMovies(JSON.parse(savedMovies));
-    }
-  }, []);
-
+  
   return (
     <>
-      <div>
-      <h1>Movie Search App</h1>
-      <SearchBar onSearch={handleSearch} />
-      <div>
-        {movies.length > 0 ? (
-          movies.map((movie) => (
-            <div key={movie.imdbID}>
-              <h3>{movie.Title}</h3>
-              <img src={movie.Poster} alt={movie.Title} width="100" />
-              <Link to={`/movie/${movie.imdbID}`}>Movie details</Link>
-            </div>
-          ))
-        ) : (
-          <p>No results found</p>
-        )}
-      </div>
-      </div>
-            
+      <LandingPage></LandingPage>
     </>
   )
 }
---
+++ src/core/components/LandingPage.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import { SearchBar } from "../components/SearchBar";
+import { fetchMovies } from "../../api/movieApi";
+import { Link } from "react-router";
+
+const LandingPage = () => {
+
+    const [movies, setMovies] = useState([]);
+
+  const handleSearch = async (query) => {
+    const results = await fetchMovies(query);
+    setMovies(results);
+    
+    if (results) {
+      setMovies(results);
+      sessionStorage.setItem("movies", JSON.stringify(results));
+    }
+  };
+
+  useEffect(() => {
+    const savedMovies = sessionStorage.getItem("movies");
+    if (savedMovies) {
+      setMovies(JSON.parse(savedMovies));
+    }
+  }, []);
+  
+  return <>{
+    <div>
+      <h1>Movie Search App</h1>
+      <SearchBar onSearch={handleSearch} />
+      <div>
+        {movies.length > 0 ? (
+          movies.map((movie) => (
+            <div key={movie.imdbID}>
+              <h3>{movie.Title}</h3>
+              <img src={movie.Poster} alt={movie.Title} width="100" />
+              <Link to={`/movie/${movie.imdbID}`}>Movie details</Link>
+            </div>
+          ))
+        ) : (
+          <p>No results found</p>
+        )}
+      </div>
+      </div>
+  }</>;
+};
+
+export default LandingPage;
